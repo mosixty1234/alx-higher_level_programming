49e1b72fc528bcd99213b60adba9d333486d753f
mosixty1234 <mosixtysipho11@gmail.com>
2023-06-11 09:35:57 -0700

    5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3 
 
-def no_c(my_string):
+def remove_characters(my_string, *chars):
     new_string = ""
     for char in my_string:
-        if char != 'c' and char != 'C':
+        if char.lower() not in [c.lower() for c in chars]:
             new_string += char
     return new_string
 
